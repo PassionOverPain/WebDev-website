2e576d366686083efe560527d762843f77f86402
Tino <tinomhedziso21@gmail.com>
2024-08-28 17:27:47 +0200

    Update email sending

--- function.js
+++ function.js
@@ -5,17 +5,17 @@ function sendEmail() {
   let bxSubject = document.getElementById("mySubject");
   let bxMessage = document.getElementById("myMessage");
 
-  emailjs.init("3NfkWXzXjcjzo_G_v");
-
   let myParams = {
-    PName: bxName.textContent,
-    PEmail: bxEmail.textContent,
-    PSubject: bxSubject.textContent,
-    PMessage: bxMessage.textContent,
+    PName: bxName.value,
+    PEmail: bxEmail.value,
+    PSubject: bxSubject.value,
+    PMessage: bxMessage.value,
   };
+
   let myServiceID = "service_wt20wpt";
   let myTemplateID = "template_fncgatu";
 
-  emailjs.send(myServiceID, myTemplateID, myParams);
-  alert("Email has been sent sir");
+  emailjs.send(myServiceID, myTemplateID, myParams).then(function () {
+    alert("Email has been sent sir");
+  });
 }
