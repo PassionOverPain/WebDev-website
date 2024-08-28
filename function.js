87e3498888fce78c7554e5111b4306ac881d1b35
Tino <tinomhedziso21@gmail.com>
2024-08-28 16:53:53 +0200

    Added send function

--- function.js
+++ function.js
@@ -1,15 +1,20 @@
 /** @format */
+function sendEmail() {
+  let bxName = document.getElementById("myName");
+  let bxEmail = document.getElementById("myEmail");
+  let bxSubject = document.getElementById("mySubject");
+  let bxMessage = document.getElementById("myMessage");
 
-let bxName = document.getElementById("myName");
-let bxEmail = document.getElementById("myEmail");
-let bxSubject = document.getElementById("mySubject");
-let bxMessage = document.getElementById("myMessage");
+  emailjs.init("3NfkWXzXjcjzo_G_v");
 
-emailjs.init("3NfkWXzXjcjzo_G_v");
+  let myParams = {
+    PName: bxName.textContent,
+    PEmail: bxEmail.textContent,
+    PSubject: bxSubject.textContent,
+    PMessage: bxMessage.textContent,
+  };
+  let myServiceID = "service_wt20wpt";
+  let myTemplateID = "template_fncgatu";
 
-let myParams = {
-  PName: bxName.textContent,
-  PEmail: bxEmail.textContent,
-  PSubject: bxSubject.textContent,
-  PMessage: bxMessage.textContent,
-};
+  emailjs.send(myServiceID, myTemplateID, myParams);
+}
