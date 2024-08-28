e59e7b39aa488633637d6250def88332762b6cd4
Tino <tinomhedziso21@gmail.com>
2024-08-28 16:49:39 +0200

    Added send paramaters

--- function.js
+++ function.js
@@ -4,3 +4,12 @@ let bxName = document.getElementById("myName");
 let bxEmail = document.getElementById("myEmail");
 let bxSubject = document.getElementById("mySubject");
 let bxMessage = document.getElementById("myMessage");
+
+emailjs.init("3NfkWXzXjcjzo_G_v");
+
+let myParams = {
+  PName: bxName.textContent,
+  PEmail: bxEmail.textContent,
+  PSubject: bxSubject.textContent,
+  PMessage: bxMessage.textContent,
+};
