00dffe289d577dc6dfeae8c84be213b70c2994fe
Tino <tinomhedziso21@gmail.com>
2024-08-28 16:36:19 +0200

    JavaScript var added

---
+++ function.js
@@ -0,0 +1,6 @@
+/** @format */
+
+let bxName = document.getElementById();
+let bxEmail = document.getElementById();
+let bxSubject = document.getElementById();
+let bxMessage = document.getElementById();
