37b16a3dcdd9ddd7c761d461041c5d513fb17691
Tino <tinomhedziso21@gmail.com>
2024-08-29 20:50:59 +0200

    added custom cursor effect

--- function.js
+++ function.js
@@ -21,3 +21,24 @@ function sendEmail() {
     );
   });
 }
+
+const cursorDot = document.getElementById("dot");
+const cursorOutline = document.getElementById("outline");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    {duration: 500, fill: "forwards"}
+  );
+});
